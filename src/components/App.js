9ec5f5ebdc27211bfcab209539654dd7e824d3ff
Phil Mass <philrmass@gmail.com>
2020-03-22 23:25:25 -0700

    add data

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import styles from '../styles/App.module.css';
+import data from '../data/cases.json';
 
 function App() {
   return (
     <div className={styles.page}>
       <Header/>
-      <div className={styles.main}>Main</div>
+      <div className={styles.main}>
+        {`${JSON.stringify(data)}`}
+      </div>
       <Footer/>
     </div>
   );
